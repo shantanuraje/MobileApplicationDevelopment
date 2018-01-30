788aaabfbe81f43cbcb357b49c17bc04220c6921
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-01-29 21:03:37 -0500

    partial edit layout dont

--- InClass3_/app/src/main/java/com/example/test/myapplication/EditActivity.java
+++ InClass3_/app/src/main/java/com/example/test/myapplication/EditActivity.java
@@ -1,10 +1,14 @@
 package com.example.test.myapplication;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
@@ -22,42 +26,30 @@ public class EditActivity extends AppCompatActivity {
         Integer requestCode = getIntent().getExtras().getInt("request_code");
         String value = getIntent().getExtras().getString("value");
 
-        /*LinearLayout layout = new LinearLayout(this);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        params.setMargins(20, 20, 20, 20);
-        layout.setLayoutParams(params);
-        layout.setOrientation(LinearLayout.VERTICAL);*/
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-        LinearLayout layout = findViewById(R.id.edit_layout);
-
-        TextView textView = new TextView(getBaseContext());
-        textView.setLayoutParams(params);
-        textView.setText(getResources().getText(R.string.edit_information));
-
-        layout.addView(textView);
+        LinearLayout edit_layout = (LinearLayout) findViewById(R.id.edit_layout);
 
         if(requestCode == DisplayActivity.editCode[0]) {
             EditText name = new EditText(getBaseContext());
-            layout.addView(name);
+            edit_layout.addView(name);
         } else if(requestCode == DisplayActivity.editCode[1]) {
-            EditText name = new EditText(getBaseContext());
-            layout.addView(name);
-        } else if(requestCode == DisplayActivity.editCode[2]) {
-
-        } else if(requestCode == DisplayActivity.editCode[3]) {
+            EditText email = new EditText(getBaseContext());
+            edit_layout.addView(email);
+        } else if (requestCode == DisplayActivity.editCode[2]){
+            RadioGroup rg_department =  new RadioGroup(getBaseContext());
+            RadioButton rb_edit_1 = new RadioButton(getBaseContext());
+            RadioButton rb_edit_2 = new RadioButton(getBaseContext());
+            RadioButton rb_edit_3 = new RadioButton(getBaseContext());
+            RadioButton rb_edit_4 = new RadioButton(getBaseContext());
+            rg_department.addView(rb_edit_1);
+            rg_department.addView(rb_edit_2);
+            rg_department.addView(rb_edit_3);
+            rg_department.addView(rb_edit_4);
+            edit_layout.addView(rg_department);
+        } else if (requestCode == DisplayActivity.editCode[3]){
+            SeekBar sb_mood = new SeekBar(getBaseContext());
+            edit_layout.addView(sb_mood);
 
         }
-
-        Button button = new Button(getBaseContext());
-        button.setLayoutParams(params);
-        button.setText(getResources().getText(R.string.save));
-
-
-
-        layout.addView(button);
-
-
+        
     }
 }
