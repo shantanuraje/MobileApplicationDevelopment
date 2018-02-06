0153f0a9a42c3f95608581404a0c34eee2380f4e
Shantanu Rajenimbalkar <shantanu.rajenimbalkar@gmail.com>
2018-02-05 19:38:39 -0500

    basic layout done

--- InClass4/app/src/main/java/mad/group12/passwordgenerator/PasswordGenerator.java
+++ InClass4/app/src/main/java/mad/group12/passwordgenerator/PasswordGenerator.java
@@ -2,6 +2,8 @@ package mad.group12.passwordgenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class PasswordGenerator extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PasswordGenerator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_generator);
+        LinearLayout ll_numOfPassCount = (LinearLayout) findViewById(R.id.ll_numOfPassCount);
+        SeekBar sb_numOfPassCount = (SeekBar) findViewById(R.id.sb_lenPass);
+
+        LinearLayout ll_lenPass = (LinearLayout) findViewById(R.id.ll_lenPass);
+        SeekBar sb_lenPass = (SeekBar)findViewById(R.id.sb_lenPass);
     }
 }
