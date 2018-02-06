03a8f8989c9c3826aa974357db357278dbce7c9c
NilInfinite <nilanjan.mhatre@gmail.com>
2018-02-05 19:01:03 -0500

    Inclass 4

---
+++ InClass4/app/src/main/java/mad/group12/passwordgenerator/PasswordDashboard.java
@@ -0,0 +1,13 @@
+package mad.group12.passwordgenerator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PasswordDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_dashboard);
+    }
+}
