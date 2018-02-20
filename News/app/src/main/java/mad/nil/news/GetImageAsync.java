affe9f315c2d2bf0428dfab870959d537decda38
NilInfinite <nilanjan.mhatre@gmail.com>
2018-02-19 19:19:26 -0500

    add files

---
+++ News/app/src/main/java/mad/nil/news/GetImageAsync.java
@@ -0,0 +1,62 @@
+package mad.nil.news;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetImageAsync extends AsyncTask<String, String, Bitmap> {
+
+    NewsFunctions newsFunctions;
+
+    public GetImageAsync(NewsFunctions newsFunctions) {
+        this.newsFunctions = newsFunctions;
+    }
+
+    @Override
+    protected Bitmap doInBackground(String... params) {
+        List<String> photoUrlList = new ArrayList<>();
+        HttpURLConnection connection = null;
+        InputStream inputStream = null;
+        String result = null;
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                inputStream = connection.getInputStream();
+                bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            newsFunctions.dismissDialog();
+            if(connection !=null) {
+                connection.disconnect();
+            }
+            if(inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bitmap;
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+        newsFunctions.displayImage(bitmap);
+        newsFunctions.dismissDialog();
+    }
+
+}
---
+++ News/app/src/main/java/mad/nil/news/News.java
@@ -0,0 +1,66 @@
+package mad.nil.news;
+
+import java.util.Date;
+
+/**
+ * Created by nilan on 2/19/2018.
+ */
+
+public class News {
+    String title;
+    Date publishedAt;
+    String imageUrl;
+    String description;
+
+    public News() {
+    }
+
+    public News(String title, Date publishedAt, String imageUrl, String description) {
+        this.title = title;
+        this.publishedAt = publishedAt;
+        this.imageUrl = imageUrl;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(Date publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "News{" +
+                "title='" + title + '\'' +
+                ", publishedAt=" + publishedAt +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
